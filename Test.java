b17ba7d193784b3994ffed05819a97287fd84cbf
Daniela <daniela@daniela-MacBookPro.(none)>
2012-11-04 22:48:04 +0100

    getting there

--- Test.java
+++ Test.java
@@ -26,6 +26,23 @@ import org.eclipse.emf.ecore.*;
 import java.io.*;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.resource.UMLResource;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.uml2.common.util.CacheAdapter;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.uml2.uml.resource.UMLResource;
+import java.util.*;
+import org.eclipse.emf.common.util.*;
+import org.eclipse.emf.ecore.impl.EAttributeImpl;
+import org.eclipse.emf.ecore.xml.type.impl.AnyTypeImpl;
+import org.eclipse.uml2.uml.internal.impl.ModelImpl;
+
 class Test {
   public static void main(String args[]) throws Exception{
     System.out.println("Success!!!");
@@ -37,10 +54,10 @@ Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(UMLResource.FI
   resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
 resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", new XMLResourceFactoryImpl());
 
-//   resourceSet.getPackageRegistry().put("http://schema.omg.org/spec/UML/2.1", UMLPackage.eINSTANCE);
+		
+  resourceSet.getPackageRegistry().put("http://schema.omg.org/spec/UML/2.1", UMLPackage.eINSTANCE);
 
   Resource resource = null;
-//       File f = new File("/home/daniela/Documents/mCRL2_new_models/September2012/mCRL2_Executors/ExportHere.xmi"); 
       File f = new File("ExportHereAgain.uml"); 
 
 
@@ -48,7 +65,29 @@ resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", n
         resource = resourceSet.getResource(uri, true);
 
     resource.load(null);
-    System.out.println("AMAAAN"+ resource.getContents().get(0) );
+//     System.out.println("AMAAAN"+ ((XMIResourceImpl)resource).getContents().get(0) );
+    
+    
+    Collection<EObject> model = new ArrayList<EObject>();
+    
+    for(TreeIterator<EObject> it = resource.getAllContents();it.hasNext();)
+    {
+	    model.add(it.next());
+    }
+    
+    
+		System.out.println("JUPII"+model.size());
+		
+Iterator itr = model.iterator(); 
+while(itr.hasNext()){
+  EObject el = (EObject)itr.next();
+  System.out.println(el);
+  System.out.println("-------------------------");
 
+// System.out.println(m);		
+// 		return model;
+//     Model m = (Model) EcoreUtil.getObjectByType(resource.getContents(), UMLPackage.Literals.MODEL);
+//     System.out.println(m);
   }
+}
 }
